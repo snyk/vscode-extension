fdf5c12162ad39c361ec9e52cd6bebf67d1c1c89
Arvid Paeglit <arvid@deepcode.ai>
2020-01-17 14:20:39 +0100

    removed unneeded setting to ignore ALL info suggestions

--- src/deepcode/lib/watchers/DeepCodeSettingsWatcher.ts
+++ src/deepcode/lib/watchers/DeepCodeSettingsWatcher.ts
@@ -2,51 +2,11 @@ import * as vscode from "vscode";
 import DeepCode from "../../../interfaces/DeepCodeInterfaces";
 import { DEEPCODE_SEVERITIES } from "../../constants/analysis";
 import {
-  HIDE_INFORMATION_ISSUES_SSETTING,
   DEEPCODE_CLOUD_BACKEND
 } from "../../constants/settings";
 
 class DeepCodeSettingsWatcher implements DeepCode.DeepCodeWatcherInterface {
-  private handleInformationIssuesStatus(
-    extension: DeepCode.ExtensionInterface
-  ): void {
-    const settings = vscode.workspace.getConfiguration();
-    const updateWorkspaceSetting = (value: boolean): void => {
-      settings.update(
-        HIDE_INFORMATION_ISSUES_SSETTING,
-        value,
-        vscode.ConfigurationTarget.Workspace
-      );
-    };
-    const hideInfoIssuesSetting = settings.inspect(
-      HIDE_INFORMATION_ISSUES_SSETTING
-    );
-
-    if (hideInfoIssuesSetting) {
-      const {
-        globalValue,
-        workspaceValue,
-        defaultValue
-      } = hideInfoIssuesSetting;
-      let hideInAnalysis: boolean = !!defaultValue;
-
-      if (globalValue) {
-        hideInAnalysis = !!globalValue;
-        updateWorkspaceSetting(hideInAnalysis);
-      } else {
-        if (workspaceValue === undefined) {
-          updateWorkspaceSetting(hideInAnalysis);
-        } else {
-          hideInAnalysis = !!workspaceValue;
-        }
-      }
-      extension.analyzer.configureIssuesDisplayBySeverity(
-        DEEPCODE_SEVERITIES.information,
-        !!hideInAnalysis
-      );
-    }
-  }
-
+  
   private prepareBackendUrlFromSettings(url: string) {
     const SLASH = "/";
     const lastletter = url.charAt(url.length - 1);
@@ -78,13 +38,8 @@ class DeepCodeSettingsWatcher implements DeepCode.DeepCodeWatcherInterface {
   }
 
   public activate(extension: DeepCode.ExtensionInterface): void {
-    this.handleInformationIssuesStatus(extension);
-
     vscode.workspace.onDidChangeConfiguration(
       (event: vscode.ConfigurationChangeEvent): void => {
-        if (event.affectsConfiguration(HIDE_INFORMATION_ISSUES_SSETTING)) {
-          this.handleInformationIssuesStatus(extension);
-        }
         if (event.affectsConfiguration(DEEPCODE_CLOUD_BACKEND)) {
           this.changeDeepCodeCloudBackend(extension);
         }
