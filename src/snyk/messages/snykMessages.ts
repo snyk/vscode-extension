f10b9b32465a486d6f3d5727eab7acab994013ec
michelkaporin <michel.kaporin@snyk.io>
2021-05-31 14:11:58 +0200

    refactor: remove redundant message

--- src/snyk/messages/snykMessages.ts
+++ src/snyk/messages/snykMessages.ts
@@ -1,11 +1,6 @@
 export const snykMessages = {
   welcome: {
-    msg: "Welcome to Snyk! Check out our dashboard to start analyzing your code",
-    button: "Check it out",
+    msg: 'Welcome to Snyk! Check out our dashboard to start analyzing your code',
+    button: 'Check it out',
   },
-  codeDisabled: {
-    msg: "Snyk Code is disabled at the organizational level settings now. Enable to keep analyzing the code.",
-    enableCode: "Enable Snyk Code",
-    remindLater: "Remind me later"
-  }
 };
