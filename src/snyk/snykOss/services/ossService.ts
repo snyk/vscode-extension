fff293f4be0474a9ccb04f3fe2aa3ac5be824608
Michel Kaporin <michelkaporin@users.noreply.github.com>
2021-09-16 16:15:54 +0200

    feat: fix CLI error handling and add checksum verification on CLI test [ROAD-285] (#55)
    * feat: work with CliError object in error handling
    
    * test: add tests for error handling
    
    * feat: add checksum verification before CLI tests
    
    * feat: ensure finalise is called to bring back the UI state
    
    * refactor: make code more readable
    
    * feat: try redownloading CLI when binary is corrupt

--- src/snyk/snykOss/services/ossService.ts
+++ src/snyk/snykOss/services/ossService.ts
@@ -1,7 +1,9 @@
-import { CliService } from '../../cli/services/cliService';
+import { CliDownloadService } from '../../cli/services/cliDownloadService';
+import { CliError, CliService } from '../../cli/services/cliService';
 import { IConfiguration } from '../../common/configuration/configuration';
 import { ILog } from '../../common/logger/interfaces';
 import { IViewManagerService } from '../../common/services/viewManagerService';
+import { ExtensionContext } from '../../common/vscode/extensionContext';
 import { IVSCodeWorkspace } from '../../common/vscode/workspace';
 import { messages } from '../messages/test';
 import { OssResult } from '../ossResult';
@@ -12,14 +14,15 @@ export class OssService extends CliService<OssResult> {
   protected readonly command: string[] = ['test'];
 
   constructor(
-    protected readonly extensionPath: string,
+    protected readonly extensionContext: ExtensionContext,
     protected readonly logger: ILog,
     protected readonly config: IConfiguration,
     private readonly suggestionProvider: ISuggestionViewProvider,
     protected readonly workspace: IVSCodeWorkspace,
     private readonly viewManagerService: IViewManagerService,
+    protected readonly downloadService: CliDownloadService
   ) {
-    super(extensionPath, logger, config, workspace);
+    super(extensionContext, logger, config, workspace, downloadService);
   }
 
   public getResult = (): OssResult | undefined => this.result;
@@ -39,8 +42,13 @@ export class OssService extends CliService<OssResult> {
     this.viewManagerService.refreshOssView();
   }
 
-  protected afterTest(): void {
-    this.logger.info(messages.testFinished);
+  protected afterTest(error?: CliError): void {
+    if (error) {
+      this.logger.error(`${messages.testFailed} ${error.error}`);
+    } else {
+      this.logger.info(messages.testFinished);
+    }
+
     this.viewManagerService.refreshOssView();
   }
 
