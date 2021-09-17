69c475d043c23fe37fe837bc5151f07ffce28e58
michelkaporin <michel.kaporin@snyk.io>
2021-09-17 15:12:29 +0200

    fix: patch script

--- scripts/patchPreview.js
+++ scripts/patchPreview.js
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import * as fs from 'fs';
+const fs = require('fs');
 
-const version = process.argv[0];
-if (version === undefined) {
+const args = process.argv.slice(2);
+if (!args || args.length == 0) {
   throw new Error('No version parameter was provided.');
 }
 
@@ -20,7 +20,7 @@ json = JSON.stringify({
   displayName: `${json.displayName} (Preview)`,
   description:
     'This is an preview release for functionality that is not yet officially released. This preview functionality will be removed in a couple of weeks and the functionality merged in the official release.',
-  version: version,
+  version: `${args[0]}-preview`,
   preview: true,
 });
 fs.writeFileSync('./package.json', json);
