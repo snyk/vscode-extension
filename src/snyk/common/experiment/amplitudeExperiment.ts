fea98bcde835e72aebc5371d0c3e73274cedad79
michelkaporin <michel.kaporin@snyk.io>
2021-11-12 08:24:53 +0100

    refactor: tidy up

--- src/snyk/common/experiment/amplitudeExperiment.ts
+++
@@ -1,7 +0,0 @@
-// import { Experiment } from '@amplitude/experiment-node-server';
-
-// export class AmplitudeExperiment {
-//   initialise(): void {
-//     this.experiment = Experiment.initialize(apiKey);
-//   }
-// }
