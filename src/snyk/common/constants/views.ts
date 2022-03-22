de2155ec81dfaee02a8edfb29dc47d8892947bad
michelkaporin <michel.kaporin@snyk.io>
2022-03-22 09:47:57 +0100

    refactor: tidy up UpdateCopyOnWelcomeView experiment

--- src/snyk/common/constants/views.ts
+++ src/snyk/common/constants/views.ts
@@ -22,7 +22,6 @@ export const SNYK_CONTEXT = {
   ERROR: 'error',
   MODE: 'mode',
   ADVANCED: 'advanced',
-  WELCOME_VIEW_EXPERIMENT: 'welcomeViewExperiment',
 };
 
 export const SNYK_ERROR_CODES = {
