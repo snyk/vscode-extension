06f964ddce47784d07e50daa0394de65efe5ee0a
michelkaporin <michel.kaporin@snyk.io>
2022-05-27 14:51:54 +0200

    feat: report anonymised user id to Sentry (ROAD-884)

--- src/test/unit/common/user.test.ts
+++ src/test/unit/common/user.test.ts
@@ -28,4 +28,13 @@ suite('User', () => {
 
     strictEqual(identifyFake.called, true);
   });
+
+  test('Returns authenticated id in SHA-256', () => {
+    const user = new User(undefined, '185958e2-6317-4543-b9bc-960d94890353');
+
+    strictEqual(
+      user.hashedAuthenticatedId?.toLowerCase(),
+      '40bd8b947f75ab4fe3f8e557761856a00a3ce421229f6cf55636b63b94a35b64',
+    );
+  });
 });
