3037810c1404845e3c8d3e6f97b6922c6b464b40
Alex Nitu <alex.nitu@snyk.io>
2022-12-06 13:34:47 +0000

    wip: autofix request

--- src/snyk/snykCode/codeActions/autofixCommand.ts
+++ src/snyk/snykCode/codeActions/autofixCommand.ts
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import * as vscode from 'vscode';
+
 import { configuration } from '../../common/configuration/instance';
 import { COMMAND_DEBOUNCE_INTERVAL } from '../../common/constants/general';
 
@@ -29,13 +30,23 @@ export const autofixIssue = _.debounce(
       return;
     }
 
-    const editorText = editor.document.getText();
-
-    console.log('Autofix issue', { uri, apiUrl: configuration.autofixBaseURL, matchedIssue, ruleId });
+    const apiUrl = configuration.autofixBaseURL;
 
     // Replace editor content.
-    // TODO: get content from API
-    const replaceContent = '<sample autofix file contents>';
+    const payload = {
+      input_code: editor.document.getText(),
+      rule_id: ruleId,
+      line_num: matchedIssue.range.start.line,
+    };
+    // Make gRPC call
+    const response = {
+      fixes: [
+        'define([], function() {\n\n  var NUM_ELEMENTS = 2;\n  var ELEMENT_BYTES = 4;\n\n  function Vec2Array(n) {\n    this.bufStorage = new ArrayBuffer(NUM_ELEMENTS * n * ELEMENT_BYTES);\n    this.buf = new Float32Array(this.bufStorage, 0, NUM_ELEMENTS * n);\n    for(var i=0; i\u003cn; i++) {\n      this[i] = new Float32Array(this.bufStorage, i * NUM_ELEMENTS * 4, NUM_ELEMENTS);\n    }\n  }\n\n  Vec2Array.prototype = Object.create(Array.prototype);\n\n  return Vec2Array;\n});',
+        'define([], function() {\n\n  var NUM_ELEMENTS = 2;\n  var ELEMENT_BYTES = 4;\n\n  function Vec2Array(n) {\n    this.bufStorage = new ArrayBuffer(NUM_ELEMENTS * n * ELEMENT_BYTES);\n    this.buf = new Float32Array(this.bufStorage, 0, NUM_ELEMENTS * n);\n    for(var i=0; i\u003cn; i++) {\n      this[i] = new Float32Array(this.bufStorage, i * NUM_ELEMENTS * 4, NUM_ELEMENTS);\n    }\n  }\n\n  Vec2Array.prototype = Object.create(Array.prototype);\n\n\n  return Vec2Array;\n});',
+      ],
+    };
+
+    const replaceContent = response.fixes[0];
     const replaceStart = editor.document.lineAt(0).range.start;
     const replaceEnd = editor.document.lineAt(editor.document.lineCount - 1).range.end;
     const replaceRange = new vscode.Range(replaceStart, replaceEnd);
