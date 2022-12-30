1a73232e83c6c78d8cb4a1a31932dd58a0ba4fd4
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-12-30 08:53:03 +0100

    fix: lint code
    Signed-off-by: Bastian Doetsch <bastian.doetsch@snyk.io>

--- src/snyk/common/configuration/trustedFolders.ts
+++ src/snyk/common/configuration/trustedFolders.ts
@@ -1,13 +1,13 @@
 import { IConfiguration } from './configuration';
 
+import path from 'path';
+
 export function getTrustedFolders(config: IConfiguration, workspaceFolders: string[]): string[] {
   const trustedFolders = config.getTrustedFolders();
-  const path = require('path');
-
   return workspaceFolders.filter(folder => {
     return trustedFolders.some(trustedFolder => {
       const relative = path.relative(trustedFolder, folder);
-      return relative && !relative.startsWith('..' + path.separator) && !path.isAbsolute(relative);
+      return relative && !relative.startsWith('..' + path.sep) && !path.isAbsolute(relative);
     });
   });
 }
