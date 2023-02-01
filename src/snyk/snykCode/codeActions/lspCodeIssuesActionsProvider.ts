29d9c017a61989e782aebcb3e962b1617d066328
Asaf Agami <asaf.agami@snyk.io>
2023-02-01 17:05:21 +0200

    fix: import errors

--- src/snyk/snykCode/codeActions/lspCodeIssuesActionsProvider.ts
+++ src/snyk/snykCode/codeActions/lspCodeIssuesActionsProvider.ts
@@ -1,25 +1,10 @@
-import {
-  CancellationToken,
-  CodeAction,
-  CodeActionContext,
-  Command,
-  ProviderResult,
-  Range,
-  Selection,
-  TextDocument,
-} from 'vscode';
-import { CodeActionProvider } from '../../common/vscode/types';
+import { CodeAction, CodeActionProvider, Range, TextDocument } from '../../common/vscode/types';
 import { CodeResult } from '../codeResult';
 
 export class LspSnykCodeIssuesActionProvider implements CodeActionProvider {
   constructor(private readonly _issues: CodeResult) {}
 
-  provideCodeActions(
-    document: TextDocument,
-    range: Range | Selection,
-    context: CodeActionContext,
-    token: CancellationToken,
-  ): ProviderResult<(CodeAction | Command)[]> {
+  public provideCodeActions(document: TextDocument, clickedRange: Range): CodeAction[] | undefined {
     const uri = document.uri;
 
     return null;
