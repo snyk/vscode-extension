96b983ac15622b3fe6e823d4114f28fb280c3c08
Bastian Doetsch <bastian.doetsch@snyk.io>
2023-04-21 13:19:28 +0530

    chore: increase protocol version to 8
    Signed-off-by: Bastian Doetsch <bastian.doetsch@snyk.io>

--- src/snyk/common/constants/languageServer.ts
+++ src/snyk/common/constants/languageServer.ts
@@ -2,7 +2,7 @@
 // Language Server name, used e.g. for the output channel
 export const SNYK_LANGUAGE_SERVER_NAME = 'Snyk Language Server';
 // The internal language server protocol version for custom messages and configuration
-export const PROTOCOL_VERSION = 7;
+export const PROTOCOL_VERSION = 8;
 
 // LS protocol methods (needed for not having to rely on vscode dependencies in testing)
 export const DID_CHANGE_CONFIGURATION_METHOD = 'workspace/didChangeConfiguration';
