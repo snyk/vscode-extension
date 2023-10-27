8138afe60c60b1e5e63139d89657a8348c617b28
Jason Luong <jason.luong@snyk.io>
2023-10-27 13:04:55 +0100

    refactor: getOpenIssueCommand abstract method to support LS OSS implementations

--- src/snyk/common/views/issueTreeProvider.ts
+++ src/snyk/common/views/issueTreeProvider.ts
@@ -42,7 +42,7 @@ export abstract class ProductIssueTreeProvider<T> extends AnalysisTreeNodeProvid
   abstract getIssueTitle(issue: Issue<T>): string;
 
   abstract getIssueRange(issue?: Issue<T>): Range | undefined;
-  abstract getOpenIssueCommand(issue: Issue<T>, folderPath: string, filePath: string): Command;
+  abstract getOpenIssueCommand(issue: Issue<T>, folderPath: string, filePath: string, filteredIssues?: Issue<T>[]): Command;
 
   getRootChildren(): TreeNode[] {
     const nodes: TreeNode[] = [];
@@ -229,7 +229,7 @@ export abstract class ProductIssueTreeProvider<T> extends AnalysisTreeNodeProvid
     return IssueSeverity.Low;
   }
 
-  private initSeverityCounts(): ISeverityCounts {
+  protected initSeverityCounts(): ISeverityCounts {
     return {
       [IssueSeverity.Critical]: 0,
       [IssueSeverity.High]: 0,
