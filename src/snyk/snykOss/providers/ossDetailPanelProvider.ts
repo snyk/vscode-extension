85ca7e48f56f8e017a3e40fc1c4976360106c0c8
PeterSchafer <101886095+PeterSchafer@users.noreply.github.com>
2023-11-10 12:33:16 +0100

    feat: Use Detail Panel from Language Server (#389)
    * docs: update instructions for contribution to include `npm run build`
    
    Signed-off-by: Bastian Doetsch <bastian.doetsch@snyk.io>
    
    * chore: use ls based oss service in cmdcontroller
    
    * chore: move convertion of Issue to OssVulnerability
    
    * chore: adapt test
    
    * chore: delete vulnerabilityCountCodeActionProviderLS class as LS handles CodeActions now
    
    * chore: delete vulnerabilityCountHoverProvider class as LS handles hovers now
    
    * chore: clean up codeaction/hovers where they are not needed anymore
    
    * fix: linting
    
    * check license data if undefined
    
    * chore: display received details html
    
    * chore: initial version taking html from ls
    
    * chore: inject learn icon
    
    * chore: inject nonce and cspsource
    
    * fix: linter
    
    ---------
    
    Signed-off-by: Bastian Doetsch <bastian.doetsch@snyk.io>
    Co-authored-by: Bastian Doetsch <bastian.doetsch@snyk.io>
    Co-authored-by: Jason Luong <jason.luong@snyk.io>

--- src/snyk/snykOss/providers/ossDetailPanelProvider.ts
+++ src/snyk/snykOss/providers/ossDetailPanelProvider.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-import { SNYK_OPEN_BROWSER_COMMAND } from '../../common/constants/commands';
 import { SNYK_VIEW_SUGGESTION_OSS_LANGUAGE_SERVER } from '../../common/constants/views';
 import { ErrorHandler } from '../../common/error/errorHandler';
 import { Issue, OssIssueData } from '../../common/languageServer/types';
@@ -17,6 +16,10 @@ export class OssDetailPanelProvider
   extends WebviewProvider<Issue<OssIssueData>>
   implements IWebViewProvider<Issue<OssIssueData>>
 {
+  protected getHtmlForWebview(_webview: vscode.Webview): string {
+    throw new Error('Method not implemented.');
+  }
+
   // For consistency reasons, the single source of truth for the current suggestion is the
   // panel state. The following field is only used in
   private issue: Issue<OssIssueData> | undefined;
@@ -63,15 +66,40 @@ export class OssDetailPanelProvider
         this.registerListeners();
       }
 
-      this.panel.webview.html = this.getHtmlForWebview(this.panel.webview);
+      const images: Record<string, string> = [
+        ['icon-code', 'svg'],
+        ['dark-critical-severity', 'svg'],
+        ['dark-high-severity', 'svg'],
+        ['dark-medium-severity', 'svg'],
+        ['dark-low-severity', 'svg'],
+        ['learn-icon', 'svg'],
+      ].reduce((accumulator: Record<string, string>, [name, ext]) => {
+        const uri = this.getWebViewUri('media', 'images', `${name}.${ext}`);
+        if (!uri) throw new Error('Image missing.');
+        accumulator[name] = uri.toString();
+        return accumulator;
+      }, {});
+
+      const displayMode = 'dark';
+      const styleUri = this.getWebViewUri('media', 'views', 'oss', 'suggestion', 'suggestion.css');
+      const headerEndValue = `<link href="${styleUri}" rel="stylesheet">`;
+      const serverityIconName = `${displayMode}-${issue.severity}-severity`;
+      const nonce = getNonce();
+
+      let html = issue.additionalData.details;
+      html = html.replace('${headerEnd}', headerEndValue);
+      html = html.replaceAll('${cspSource}', this.panel.webview.cspSource);
+      html = html.replaceAll('${nonce}', nonce);
+      html = html.replace('${severityIcon}', images[serverityIconName]);
+      html = html.replace('${learnIcon}', images['learn-icon']);
+      html = html.replaceAll(/\$\{\w+\}/g, '');
+      this.panel.webview.html = html;
       this.panel.iconPath = vscode.Uri.joinPath(
         vscode.Uri.file(this.context.extensionPath),
         'media',
         'images',
-        'snyk-iac.svg', // TODO: USE OSS ICON
+        'snyk-oss.svg',
       );
-
-      await this.panel.webview.postMessage({ type: 'set', args: issue });
 
       this.issue = issue;
     } catch (e) {
@@ -84,8 +112,6 @@ export class OssDetailPanelProvider
 
     this.panel.onDidDispose(() => this.onPanelDispose(), null, this.disposables);
     this.panel.onDidChangeViewState(() => this.checkVisibility(), undefined, this.disposables);
-    // Handle messages from the webview
-    this.panel.webview.onDidReceiveMessage(msg => this.handleMessage(msg), undefined, this.disposables);
   }
 
   disposePanel(): void {
@@ -94,109 +120,5 @@ export class OssDetailPanelProvider
 
   protected onPanelDispose(): void {
     super.onPanelDispose();
-  }
-
-  private async handleMessage(message: any) {
-    try {
-      const { type, value } = message;
-      switch (type) {
-        case 'openBrowser': {
-          await vscode.commands.executeCommand(SNYK_OPEN_BROWSER_COMMAND, value);
-          break;
-        }
-        default: {
-          throw new Error('Unknown message type');
-        }
-      }
-    } catch (e) {
-      ErrorHandler.handle(e, this.logger, errorMessages.suggestionViewShowFailed);
-    }
-  }
-
-  protected getHtmlForWebview(webview: vscode.Webview): string {
-    const images: Record<string, string> = [
-      ['dark-critical-severity', 'svg'],
-      ['dark-high-severity', 'svg'],
-      ['dark-medium-severity', 'svg'],
-      ['dark-low-severity', 'svg'],
-    ].reduce((accumulator: Record<string, string>, [name, ext]) => {
-      const uri = this.getWebViewUri('media', 'images', `${name}.${ext}`);
-      if (!uri) throw new Error('Image missing.');
-      accumulator[name] = uri.toString();
-      return accumulator;
-    }, {});
-
-    const scriptUri = this.getWebViewUri(
-      'out',
-      'snyk',
-      'snykIac',
-      'views',
-      'suggestion',
-      'iacSuggestionWebviewScript.js',
-    );
-    const styleUri = this.getWebViewUri('media', 'views', 'oss', 'suggestion', 'suggestion.css'); // make it common
-
-    const nonce = getNonce();
-
-    return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-				<!--
-					Use a content security policy to only allow loading images from https or from our extension directory,
-					and only allow scripts that have a specific nonce.
-				-->
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
-
-				<link href="${styleUri}" rel="stylesheet">
-			</head>
-			<body>
-        <div class="suggestion">
-          <section class="suggestion--header">
-            <div class="severity">
-              <img id="lowl" class="icon light-only hidden" src="${images['dark-low-severity']}" />
-              <img id="lowd" class="icon dark-only hidden" src="${images['dark-low-severity']}" />
-              <img id="mediuml" class="icon light-only hidden" src="${images['dark-medium-severity']}" />
-              <img id="mediumd" class="icon dark-only hidden" src="${images['dark-medium-severity']}" />
-              <img id="highl" class="icon light-only hidden" src="${images['dark-high-severity']}" />
-              <img id="highd" class="icon dark-only hidden" src="${images['dark-high-severity']}" />
-              <img id="criticall" class="icon light-only hidden" src="${images['dark-critical-severity']}" />
-              <img id="criticald" class="icon dark-only hidden" src="${images['dark-critical-severity']}" />
-              <span id="severity-text"></span>
-            </div>
-            <div class="suggestion-text"></div>
-            <div class="identifiers"></div>
-          </section>
-          <section class="delimiter-top summary">
-            <div class="summary-item description">
-              <div class="label font-light">Description</div>
-              <div class="content"></div>
-            </div>
-            <div class="summary-item impact">
-              <div class="label font-light">Impact</div>
-              <div class="content"></div>
-            </div>
-            <div class="summary-item path">
-              <div class="label font-light">Path</div>
-              <div class="content">
-                <code></code>
-              </div>
-            </div>
-          </section>
-        </div>
-          <section class="delimiter-top">
-            <h2>Remediation</h2>
-            <div class="remediation" class="font-light"></div>
-          </section>
-          <section class="delimiter-top hidden references">
-            <h2>References</h2>
-            <div class="reference-links" class="font-light"></div>
-          </section>
-        </div>
-				<script nonce="${nonce}" src="${scriptUri}"></script>
-			</body>
-			</html>`;
   }
 }
