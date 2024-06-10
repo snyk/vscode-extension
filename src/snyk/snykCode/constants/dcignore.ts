d6c78d82553288573c2db6427c769b7dc0f6a0ca
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2024-06-10 16:50:57 +0200

    refactor: use dcignore package

--- src/snyk/snykCode/constants/dcignore.ts
+++ src/snyk/snykCode/constants/dcignore.ts
@@ -1,26 +1,33 @@
-export declare const MAX_PAYLOAD: number;
-export declare const MAX_FILE_SIZE: number;
-export declare const HASH_ALGORITHM = 'sha256';
-export declare const ENCODE_TYPE = 'hex';
-export declare const GIT_FILENAME = '.git';
-export declare const GITIGNORE_FILENAME = '.gitignore';
-export declare const DCIGNORE_FILENAME = '.dcignore';
-export declare const DOTSNYK_FILENAME = '.snyk';
-export declare const EXCLUDED_NAMES: string[];
-export declare const CACHE_KEY = '.dccache';
-export declare const MAX_UPLOAD_ATTEMPTS = 10;
-export declare const UPLOAD_CONCURRENCY = 2;
-export declare const POLLING_INTERVAL = 500;
-export declare const MAX_RETRY_ATTEMPTS = 10;
-export declare const REQUEST_RETRY_DELAY: number;
-export declare const ORG_ID_REGEXP: RegExp;
-export declare const IGNORES_DEFAULT: string[];
-export declare const IGNORE_FILES_NAMES: string[];
-export declare const DCIGNORE_DRAFTS: {
-  custom: string;
-  default: string;
+import { CustomDCIgnore, DefaultDCIgnore } from '@deepcode/dcignore';
+
+export const MAX_PAYLOAD = 4 * 1024 * 1024;
+export const MAX_FILE_SIZE = 1024 * 1024;
+export const HASH_ALGORITHM = 'sha256';
+export const ENCODE_TYPE = 'hex';
+export const GIT_FILENAME = '.git';
+export const GITIGNORE_FILENAME = '.gitignore';
+export const DCIGNORE_FILENAME = '.dcignore';
+export const DOTSNYK_FILENAME = '.snyk';
+export const EXCLUDED_NAMES = [GITIGNORE_FILENAME, DCIGNORE_FILENAME];
+export const CACHE_KEY = '.dccache';
+export const MAX_UPLOAD_ATTEMPTS = 10;
+export const UPLOAD_CONCURRENCY = 2;
+export const POLLING_INTERVAL = 500;
+export const MAX_RETRY_ATTEMPTS = 10; // Request retries on network errors
+export const REQUEST_RETRY_DELAY = 5 * 1000; // delay between retries in milliseconds
+export const ORG_ID_REGEXP = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
+
+export const IGNORES_DEFAULT = [`**/${GIT_FILENAME}/**`];
+
+export const IGNORE_FILES_NAMES = [GITIGNORE_FILENAME, DCIGNORE_FILENAME, DOTSNYK_FILENAME];
+
+export const DCIGNORE_DRAFTS = {
+  custom: CustomDCIgnore,
+  default: DefaultDCIgnore,
 };
-export declare enum ErrorCodes {
+
+// eslint-disable-next-line no-shadow
+export enum ErrorCodes {
   loginInProgress = 304,
   badRequest = 400,
   unauthorizedUser = 401,
@@ -34,17 +41,31 @@ export declare enum ErrorCodes {
   serviceUnavailable = 503,
   timeout = 504,
 }
-export declare const NETWORK_ERRORS: {
-  ETIMEDOUT: ErrorCodes;
-  ECONNREFUSED: ErrorCodes;
-  ECONNRESET: ErrorCodes;
-  ENETUNREACH: ErrorCodes;
-  ENOTFOUND: ErrorCodes;
+
+export const NETWORK_ERRORS = {
+  ETIMEDOUT: ErrorCodes.timeout,
+  ECONNREFUSED: ErrorCodes.connectionRefused,
+  ECONNRESET: ErrorCodes.connectionRefused,
+  ENETUNREACH: ErrorCodes.connectionRefused,
+  ENOTFOUND: ErrorCodes.dnsNotFound,
 };
-export declare const DEFAULT_ERROR_MESSAGES: {
-  [P in ErrorCodes]: string;
+
+export const DEFAULT_ERROR_MESSAGES: { [P in ErrorCodes]: string } = {
+  [ErrorCodes.serverError]: 'Unexpected server error', // 500
+  [ErrorCodes.badGateway]: 'Bad gateway', // 502
+  [ErrorCodes.serviceUnavailable]: 'Service unavailable', // 503
+  [ErrorCodes.timeout]: 'Timeout occured. Try again later.', // 504
+  [ErrorCodes.dnsNotFound]: '[Connection issue] Could not resolve domain', // 452
+  [ErrorCodes.connectionRefused]: '[Connection issue] Connection refused', // 421
+  [ErrorCodes.loginInProgress]: 'Login has not been confirmed yet',
+  [ErrorCodes.badRequest]: 'Bad request',
+  [ErrorCodes.unauthorizedUser]: 'Missing, revoked or inactive token',
+  [ErrorCodes.unauthorizedBundleAccess]: 'Unauthorized access to requested bundle analysis',
+  [ErrorCodes.notFound]: 'Not found',
+  [ErrorCodes.bigPayload]: `Payload too large (max is ${MAX_PAYLOAD}b)`,
 };
-export declare type GenericErrorTypes =
+
+export type GenericErrorTypes =
   | ErrorCodes.serverError
   | ErrorCodes.badGateway
   | ErrorCodes.serviceUnavailable
