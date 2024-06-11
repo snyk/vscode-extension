3cfd2543a9f6ad5e1ac4da17f76f0584a75cf433
Teodora Sandu <81559517+teodora-sandu@users.noreply.github.com>
2024-06-11 13:17:23 +0100

    feat: ai fix html (#467)
    * feat: ai fix html
    
    * fix: add event listener conditionally

--- src/snyk/snykCode/views/suggestion/codeSuggestionWebviewScriptLS.ts
+++ src/snyk/snykCode/views/suggestion/codeSuggestionWebviewScriptLS.ts
@@ -21,6 +21,10 @@
     file: string;
   };
   type Point = [number, number];
+  type AutofixUnifiedDiffSuggestion = {
+    fixId: string;
+    unifiedDiffsPerFile: { [key: string]: string };
+  };
   type Suggestion = {
     id: string;
     message: string;
@@ -32,8 +36,9 @@
     markers?: Marker[];
     cols: Point;
     rows: Point;
-    hasAIFix: boolean;
     filePath: string;
+    hasAIFix: boolean;
+    diffs: AutofixUnifiedDiffSuggestion[];
   };
 
   type OpenLocalMessage = {
@@ -69,7 +74,45 @@
     type: 'get';
   };
 
-  type SuggestionMessage = OpenLocalMessage | IgnoreIssueMessage | SetSuggestionMessage | GetSuggestionMessage;
+  type GetAutofixDiffsMesssage = {
+    type: 'getAutofixDiffs';
+    args: {
+      suggestion: Suggestion;
+    };
+  };
+
+  type ApplyGitDiffMessage = {
+    type: 'applyGitDiff';
+    args: {
+      patch: string;
+      filePath: string;
+    };
+  };
+
+  type SetAutofixDiffsMessage = {
+    type: 'setAutofixDiffs';
+    args: {
+      suggestion: Suggestion;
+      diffs: AutofixUnifiedDiffSuggestion[];
+    };
+  };
+
+  type SetAutofixErrorMessage = {
+    type: 'setAutofixError';
+    args: {
+      suggestion: Suggestion;
+    };
+  };
+
+  type SuggestionMessage =
+    | OpenLocalMessage
+    | IgnoreIssueMessage
+    | SetSuggestionMessage
+    | GetSuggestionMessage
+    | GetAutofixDiffsMesssage
+    | ApplyGitDiffMessage
+    | SetAutofixDiffsMessage
+    | SetAutofixErrorMessage;
 
   const vscode = acquireVsCodeApi();
 
@@ -77,8 +120,20 @@
     vscode.postMessage(message);
   }
 
-  function navigateToIssue(_e: any, position: MarkerPosition) {
-    if (!suggestion) return;
+  function navigateToIssue(i: number) {
+    if (!suggestion) {
+      return;
+    }
+    const markers = suggestion.markers;
+    if (!markers) {
+      return;
+    }
+    const position = {
+      file: suggestion.filePath,
+      rows: markers[i].pos[0].rows,
+      cols: markers[i].pos[0].cols,
+    };
+
     const message: OpenLocalMessage = {
       type: 'openLocal',
       args: {
@@ -119,27 +174,178 @@
 
   const dataFlows = document.getElementsByClassName('data-flow-clickable-row');
   for (let i = 0; i < dataFlows.length; i++) {
-    dataFlows[i].addEventListener('click', e => {
-      if (!suggestion) {
-        return;
-      }
-      const markers = suggestion.markers;
-      if (!markers) {
-        return;
-      }
-      navigateToIssue(e, {
-        file: suggestion.filePath,
-        rows: markers[i].pos[0].rows,
-        cols: markers[i].pos[0].cols,
-      });
+    dataFlows[i].addEventListener('click', () => {
+      navigateToIssue(i);
     });
   }
-  document.getElementById('ignore-line-issue')!.addEventListener('click', () => {
+  document.getElementById('ignore-line-issue')?.addEventListener('click', () => {
     ignoreIssue(true);
   });
-  document.getElementById('ignore-file-issue')!.addEventListener('click', () => {
+  document.getElementById('ignore-file-issue')?.addEventListener('click', () => {
     ignoreIssue(false);
   });
+  document.getElementById('position-line')!.addEventListener('click', () => {
+    navigateToIssue(0);
+  });
+
+  function toggleElement(element: Element | null, toggle: 'hide' | 'show') {
+    if (!element) {
+      return;
+    }
+
+    if (toggle === 'show') {
+      element.classList.remove('hidden');
+    } else if (toggle === 'hide') {
+      element.classList.add('hidden');
+    } else {
+      console.error('Unexpected toggle value', toggle);
+    }
+  }
+
+  // different AI fix buttons
+  const applyFixButton = document.getElementById('apply-fix') as HTMLElement;
+  const retryGenerateFixButton = document.getElementById('retry-generate-fix') as HTMLElement;
+  const generateAIFixButton = document.getElementById('generate-ai-fix') as HTMLElement;
+
+  function generateAIFix() {
+    if (!suggestion) {
+      return;
+    }
+
+    toggleElement(generateAIFixButton, 'hide');
+    toggleElement(fixLoadingIndicatorElem, 'show');
+    const message: GetAutofixDiffsMesssage = {
+      type: 'getAutofixDiffs',
+      args: { suggestion },
+    };
+    sendMessage(message);
+  }
+
+  function retryGenerateAIFix() {
+    console.log('retrying generate AI Fix');
+
+    toggleElement(fixWrapperElem, 'show');
+    toggleElement(fixErrorSectionElem, 'hide');
+
+    generateAIFix();
+  }
+
+  function applyFix() {
+    if (!suggestion) return;
+    const diffSuggestion = suggestion.diffs[diffSelectedIndex];
+    const filePath = suggestion.filePath;
+    const patch = diffSuggestion.unifiedDiffsPerFile[filePath];
+
+    const message: ApplyGitDiffMessage = {
+      type: 'applyGitDiff',
+      args: { filePath, patch },
+    };
+    sendMessage(message);
+  }
+
+  generateAIFixButton?.addEventListener('click', generateAIFix);
+  retryGenerateFixButton?.addEventListener('click', retryGenerateAIFix);
+  applyFixButton?.addEventListener('click', applyFix);
+
+  // different AI fix states
+  const fixLoadingIndicatorElem = document.getElementById('fix-loading-indicator') as HTMLElement;
+  const fixWrapperElem = document.getElementById('fix-wrapper') as HTMLElement;
+  const fixSectionElem = document.getElementById('fixes-section') as HTMLElement;
+  const fixErrorSectionElem = document.getElementById('fixes-error-section') as HTMLElement;
+
+  // generated AI fix diffs
+  const nextDiffElem = document.getElementById('next-diff') as HTMLElement;
+  const previousDiffElem = document.getElementById('previous-diff') as HTMLElement;
+  const diffSelectedIndexElem = document.getElementById('diff-counter') as HTMLElement;
+
+  const diffTopElem = document.getElementById('diff-top') as HTMLElement;
+  const diffElem = document.getElementById('diff') as HTMLElement;
+  const noDiffsElem = document.getElementById('info-no-diffs') as HTMLElement;
+
+  const diffNumElem = document.getElementById('diff-number') as HTMLElement;
+  const diffNum2Elem = document.getElementById('diff-number2') as HTMLElement;
+
+  let diffSelectedIndex = 0;
+
+  function nextDiff() {
+    if (!suggestion || !suggestion.diffs || diffSelectedIndex >= suggestion.diffs.length - 1) return;
+    ++diffSelectedIndex;
+    showCurrentDiff();
+  }
+
+  function previousDiff() {
+    if (!suggestion || !suggestion.diffs || diffSelectedIndex <= 0) return;
+    --diffSelectedIndex;
+    showCurrentDiff();
+  }
+
+  function showCurrentDiff() {
+    if (!suggestion?.diffs?.length || diffSelectedIndex < 0 || diffSelectedIndex >= suggestion.diffs.length) return;
+
+    toggleElement(noDiffsElem, 'hide');
+    toggleElement(diffTopElem, 'show');
+    toggleElement(diffElem, 'show');
+
+    diffNumElem.innerText = suggestion.diffs.length.toString();
+    diffNum2Elem.innerText = suggestion.diffs.length.toString();
+
+    diffSelectedIndexElem.innerText = (diffSelectedIndex + 1).toString();
+
+    const diffSuggestion = suggestion.diffs[diffSelectedIndex];
+
+    const filePath = suggestion.filePath;
+    const patch = diffSuggestion.unifiedDiffsPerFile[filePath];
+
+    // clear all elements
+    while (diffElem.firstChild) {
+      diffElem.removeChild(diffElem.firstChild);
+    }
+    diffElem.appendChild(generateDiffHtml(patch));
+  }
+
+  function generateDiffHtml(patch: string): HTMLElement {
+    const codeLines = patch.split('\n');
+
+    // the first two lines are the file names
+    codeLines.shift();
+    codeLines.shift();
+
+    const diffHtml = document.createElement('div');
+    let blockDiv: HTMLElement | null = null;
+
+    for (const line of codeLines) {
+      if (line.startsWith('@@ ')) {
+        blockDiv = document.createElement('div');
+        blockDiv.className = 'example';
+
+        if (blockDiv) {
+          diffHtml.appendChild(blockDiv);
+        }
+      } else {
+        const lineDiv = document.createElement('div');
+        lineDiv.className = 'example-line';
+
+        if (line.startsWith('+')) {
+          lineDiv.classList.add('added');
+        } else if (line.startsWith('-')) {
+          lineDiv.classList.add('removed');
+        }
+
+        const lineCode = document.createElement('code');
+        // if line is empty, we need to fallback to ' '
+        // to make sure it displays in the diff
+        lineCode.innerText = line.slice(1, line.length) || ' ';
+
+        lineDiv.appendChild(lineCode);
+        blockDiv?.appendChild(lineDiv);
+      }
+    }
+
+    return diffHtml;
+  }
+
+  nextDiffElem.addEventListener('click', nextDiff);
+  previousDiffElem.addEventListener('click', previousDiff);
 
   window.addEventListener('message', event => {
     const message = event.data as SuggestionMessage;
@@ -155,6 +361,30 @@
           suggestion = newSuggestion;
         }
         break;
+      }
+      case 'setAutofixDiffs': {
+        if (suggestion?.id === message.args.suggestion.id) {
+          toggleElement(fixSectionElem, 'show');
+          toggleElement(fixLoadingIndicatorElem, 'hide');
+          toggleElement(fixWrapperElem, 'hide');
+
+          const { diffs } = message.args;
+          suggestion.diffs = diffs;
+
+          vscode.setState({ ...vscode.getState(), suggestion });
+          showCurrentDiff();
+        }
+        break;
+      }
+      case 'setAutofixError': {
+        const errorSuggestion = message.args.suggestion;
+
+        if (errorSuggestion.id != suggestion?.id) {
+          console.log('Got an error for a previously generated suggestion: ignoring');
+          break;
+        }
+        toggleElement(fixWrapperElem, 'hide');
+        toggleElement(fixErrorSectionElem, 'show');
       }
     }
   });
