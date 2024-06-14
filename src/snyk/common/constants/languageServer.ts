f53dae716963429a2abf4bf17a9ce9edac10ed37
Bastian Doetsch <bastian.doetsch@snyk.io>
2024-06-14 08:26:26 +0200

    chore: update protocol version to 12

--- src/snyk/common/constants/languageServer.ts
+++ src/snyk/common/constants/languageServer.ts
@@ -2,7 +2,7 @@
 // Language Server name, used e.g. for the output channel
 export const SNYK_LANGUAGE_SERVER_NAME = 'Snyk Language Server';
 // The internal language server protocol version for custom messages and configuration
-export const PROTOCOL_VERSION = 11;
+export const PROTOCOL_VERSION = 12;
 
 // LS protocol methods (needed for not having to rely on vscode dependencies in testing)
 export const DID_CHANGE_CONFIGURATION_METHOD = 'workspace/didChangeConfiguration';
