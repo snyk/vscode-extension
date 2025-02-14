7b65be4de996784a2c09fa2228d5a1a8268b3ae0
Felix Sargent <felix.sargent@gmail.com>
2025-02-14 10:59:45 +0000

    fix: removes the dependency on dcignore to create the .dcignore files.

---
+++ src/snyk/snykCode/constants/empty.dcignore.ts
@@ -0,0 +1,5 @@
+const CustomDCIgnore = `# Write glob rules for ignored files.
+# For more information, see https://docs.snyk.io/scm-ide-and-ci-cd-integrations/snyk-ide-plugins-and-extensions/visual-studio-code-extension/create-a-.dcignore-file
+# Check examples on https://github.com/github/gitignore`;
+
+export default CustomDCIgnore;
