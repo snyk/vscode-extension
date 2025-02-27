c7bd397b7f07735b93538ad5f5ca8abddc211fd2
Bastian Doetsch <bastian.doetsch@snyk.io>
2025-02-27 11:01:33 +0100

    feat: display results in table, add show command

--- src/snyk/common/languageServer/languageServer.ts
+++ src/snyk/common/languageServer/languageServer.ts
@@ -23,7 +23,7 @@ import { IVSCodeWindow } from '../vscode/window';
 import { IVSCodeWorkspace } from '../vscode/workspace';
 import { LanguageClientMiddleware } from './middleware';
 import { LanguageServerSettings, ServerSettings } from './settings';
-import { CodeIssueData, IacIssueData, OssIssueData, Scan, ScanProduct, ScanStatus } from './types';
+import { CodeIssueData, IacIssueData, Issue, OssIssueData, Scan, ScanProduct, ScanStatus } from './types';
 import { ExtensionContext } from '../vscode/extensionContext';
 import { ISummaryProviderService } from '../../base/summary/summaryProviderService';
 import { ChatRequest, ChatResponseStream, CommandDetail, CommandProvider, GeminiCodeAssist } from '../llm/geminiApi';
@@ -34,7 +34,8 @@ import { UriAdapter } from '../vscode/uri';
 import path from 'path';
 import { vsCodeCommands } from '../vscode/commands';
 import { DiagnosticsIssueProvider } from '../services/diagnosticsService';
-import { baseSchema } from '@amplitude/ampli/lib/settings/schema';
+import { configuration } from '../configuration/instance';
+import { integer } from 'vscode-languageclient';
 
 export interface ILanguageServer {
   start(): Promise<void>;
@@ -250,6 +251,11 @@ export class LanguageServer implements ILanguageServer {
               description: 'Perform a workspace scan with the Snyk Security Extension',
               icon: iconPath,
             } as CommandDetail,
+            {
+              command: 'show',
+              description: 'Show issues know to the Snyk Security Extension',
+              icon: iconPath,
+            } as CommandDetail,
           ];
           return Promise.resolve(commands);
         },
@@ -266,45 +272,95 @@ export class LanguageServer implements ILanguageServer {
       this.logger.debug('received chat request from gemini: ' + request.prompt.fullPrompt());
       if (token.isCancellationRequested) return Promise.resolve();
 
-      if (!request.prompt.fullPrompt().includes('/scan')) {
+      if (!request.prompt.fullPrompt().includes('/scan') && !request.prompt.fullPrompt().includes('show')) {
         return Promise.resolve();
       }
-
-      const mdsa = new MarkdownStringAdapter();
-      responseStream.push(mdsa.get('Scanning workspace with Snyk...'));
 
       const diagnosticsIssueProvider = new DiagnosticsIssueProvider();
+      const mdsa = new MarkdownStringAdapter();
 
-      // subscribe to snyk code
-      this.scan$.subscribe((scan: Scan<CodeIssueData | OssIssueData | IacIssueData>) => {
-        const msg = 'Scan status for ' + scan.folderPath + ': ' + scan.status + '.';
-        responseStream.push(mdsa.get(msg));
-        if (scan.status == ScanStatus.Success) {
-          const issues = diagnosticsIssueProvider.getIssuesFromDiagnostics(scan.product);
-          let issueMsg = '\n\n## Snyk Issues\n';
-          for (const issue of issues) {
-            const baseName = path.basename(issue.filePath);
-            issueMsg += '*' + issue.severity + '* `' + issue.title + '` in ' + baseName + '\n\n';
+      if (request.prompt.fullPrompt().includes('/scan')) {
+        responseStream.push(mdsa.get('Scanning workspace with Snyk...'));
+
+        let openScansCount = this.countEnabledProducts();
+
+        // subscribe to snyk scan topic to get issue data
+        this.scan$.subscribe((scan: Scan<CodeIssueData | OssIssueData | IacIssueData>) => {
+          const msg = 'Scan status for ' + scan.folderPath + ': ' + scan.status + '.';
+          responseStream.push(mdsa.get(msg));
+
+          if (scan.status == ScanStatus.Success || scan.status == ScanStatus.Error) {
+            openScansCount--;
           }
-          responseStream.push(mdsa.get(issueMsg));
-          // todo add button to open in Snyk UI
-          // todo push to context
+        });
+
+        await vsCodeCommands.executeCommand(SNYK_EXECUTE_MCP_TOOL_COMMAND, SNYK_WORKSPACE_SCAN_COMMAND);
+        while (openScansCount > 0) {
+          // eslint-disable-next-line no-await-in-loop
+          await new Promise(resolve => setTimeout(resolve, 100));
         }
-      });
-
-      const result: string | undefined = await vsCodeCommands.executeCommand(
-        SNYK_EXECUTE_MCP_TOOL_COMMAND,
-        SNYK_WORKSPACE_SCAN_COMMAND,
-      );
-
-      if (!result) {
-        return Promise.resolve();
       }
 
-      const markdown = mdsa.get(result, true);
+      // show
+      const markdown = mdsa.get(this.getIssueMarkDownFromDiagnostics(diagnosticsIssueProvider), true);
       responseStream.push(markdown);
-
+      responseStream.close();
       return Promise.resolve();
     };
+  }
+
+  private countEnabledProducts(): integer {
+    const enabledProducts = [];
+    if (configuration.getFeaturesConfiguration()?.ossEnabled) enabledProducts.push(ScanProduct.OpenSource);
+    if (
+      configuration.getFeaturesConfiguration()?.codeSecurityEnabled ||
+      configuration.getFeaturesConfiguration()?.codeQualityEnabled
+    )
+      enabledProducts.push(ScanProduct.Code);
+
+    if (configuration.getFeaturesConfiguration()?.iacEnabled) enabledProducts.push(ScanProduct.InfrastructureAsCode);
+    return enabledProducts.length;
+  }
+
+  private getIssueMarkDownFromDiagnostics(diagnosticsIssueProvider: DiagnosticsIssueProvider<unknown>): string {
+    let issueMsg = 'No issues found.';
+    try {
+      const codeIssues = diagnosticsIssueProvider.getIssuesFromDiagnostics(ScanProduct.Code);
+      const ossIssues = diagnosticsIssueProvider.getIssuesFromDiagnostics(ScanProduct.OpenSource);
+      const iacIssues = diagnosticsIssueProvider.getIssuesFromDiagnostics(ScanProduct.InfrastructureAsCode);
+      if (codeIssues.length > 0 || ossIssues.length > 0 || iacIssues.length > 0) {
+        issueMsg = '';
+      }
+      if (codeIssues.length > 0) {
+        issueMsg += '\n\n## Snyk ' + ScanProduct.Code + 'Issues\n';
+        issueMsg += '| Severity | Title | Filename |\n|----------|----------|----------|\n';
+        for (const issue of codeIssues) {
+          issueMsg += this.enrichMessageWithIssueData(issue);
+        }
+      }
+      if (ossIssues.length > 0) {
+        issueMsg += '\n\n## Snyk ' + ScanProduct.OpenSource + 'Issues\n';
+        issueMsg += '| Severity | Title | Filename |\n|----------|----------|----------|\n';
+        for (const issue of ossIssues) {
+          issueMsg += this.enrichMessageWithIssueData(issue);
+        }
+      }
+      if (iacIssues.length > 0) {
+        issueMsg += '\n\n## Snyk ' + ScanProduct.InfrastructureAsCode + 'Issues\n';
+        issueMsg += '| Severity | Title | Filename |\n|----------|----------|----------|\n';
+        for (const issue of iacIssues) {
+          issueMsg += this.enrichMessageWithIssueData(issue);
+        }
+      }
+    } catch (e) {
+      ErrorHandler.handle(e, this.logger, e instanceof Error ? e.message : 'An error occurred');
+    }
+    return issueMsg;
+  }
+
+  private enrichMessageWithIssueData(issue: Issue<unknown>) {
+    // todo nicer format
+    const baseName = path.basename(issue.filePath);
+    return '| ' + issue.severity + ' | ' + issue.title + ' | ' + baseName + ' | \n';
   }
 }
