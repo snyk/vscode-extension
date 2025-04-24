96ae3892dd4079fbbd29684bc3910b0cef8e0e12
Ben Durrans <Benjamin.Durrans@snyk.io>
2025-04-24 15:49:58 +0100

    feat: handle IAW creation form
    Handle the values from the JS provided by LS and call the `snyk.submitIgnoreRequest` LS command.

---
+++ src/snyk/snykCode/views/suggestion/ideFuncs/ideSubmitIgnoreRequest.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+type SubmitIgnoreRequestMessage = {
+  type: 'submitIgnoreRequest';
+  args: {
+    id: string;
+    ignoreType: string;
+    ignoreExpirationDate: string;
+    ignoreReason: string;
+  };
+};
+
+const submitIgnoreRequestMessage: SubmitIgnoreRequestMessage = {
+  type: 'submitIgnoreRequest',
+  args: {
+    // @ts-expect-error this will be injected from a func coming from LS
+    id: suggestion.id,
+    // @ts-expect-error this will be injected from a func coming from LS
+    ignoreType: ignoreType,
+    // @ts-expect-error this will be injected from a func coming from LS
+    ignoreExpirationDate: ignoreExpirationDate,
+    // @ts-expect-error this will be injected from a func coming from LS
+    ignoreReason: ignoreReason,
+  },
+};
+vscode.postMessage(submitIgnoreRequestMessage);
