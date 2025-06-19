b273d467d054167320fe43b54d551b8b86a2a014
Ben Durrans <Benjamin.Durrans@snyk.io>
2025-06-19 16:52:40 +0100

    chore: remove deprecated inline JS package vulns

--- src/snyk/common/parsing.ts
+++ src/snyk/common/parsing.ts
@@ -1,22 +1,10 @@
 import npmValidPackageName from 'validate-npm-package-name';
-import {
-  JAVASCRIPT,
-  JAVASCRIPT_FILE_REGEX,
-  JAVASCRIPT_REACT,
-  PJSON,
-  TYPESCRIPT,
-  TYPESCRIPT_FILE_REGEX,
-  TYPESCRIPT_REACT,
-} from './constants/languageConsts';
+import { PJSON } from './constants/languageConsts';
 import nativeModules from './constants/nativeModules';
 import { ImportedModule, Language } from './types';
 
 export function getSupportedLanguage(fileName: string, languageId: string): Language | null {
-  if (languageId === TYPESCRIPT || languageId === TYPESCRIPT_REACT || TYPESCRIPT_FILE_REGEX.test(fileName)) {
-    return Language.TypeScript;
-  } else if (languageId === JAVASCRIPT || languageId === JAVASCRIPT_REACT || JAVASCRIPT_FILE_REGEX.test(fileName)) {
-    return Language.JavaScript;
-  } else if (languageId === PJSON && fileName.endsWith('package.json')) {
+  if (languageId === PJSON && fileName.endsWith('package.json')) {
     return Language.PJSON;
   }
 
