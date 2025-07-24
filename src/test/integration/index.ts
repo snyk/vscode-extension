a2ef406d8fa2d7d1e1d47355e3d84745e4ba79f6
Ben Durrans <Benjamin.Durrans@snyk.io>
2025-07-24 10:46:19 +0100

    chore: pass mocha args for test:integration
    Now you can run things like `npm run test:integration -- --grep 'some testcase'`.

--- src/test/integration/index.ts
+++ src/test/integration/index.ts
@@ -1,14 +1,40 @@
 import { glob } from 'glob';
 import Mocha from 'mocha';
 import path from 'path';
+import { isStringArray } from '../../snyk/common/tsUtil';
+// @ts-expect-error - Internal Mocha module without TypeScript declarations
+import { loadOptions } from 'mocha/lib/cli/cli';
 
 export async function run(): Promise<void> {
-  // Create the mocha test
-  const mocha = new Mocha({
+  // Our set Mocha options
+  const baseOptions: Mocha.MochaOptions = {
     ui: 'tdd',
     color: true,
     slow: 150, // double the default, as integration tests are slow
     timeout: process.env.SNYK_VSCE_DEVELOPMENT ? 60000 : undefined,
+  };
+
+  // Parse additional options from environment variable
+  const mochaCliArgsJSONStringified = process.env.TEST_MOCHA_CLI_ARGS;
+  let additionalOptions: Mocha.MochaOptions = {};
+  if (mochaCliArgsJSONStringified) {
+    try {
+      const mochaCliArgsArray = JSON.parse(mochaCliArgsJSONStringified) as unknown;
+      if (!isStringArray(mochaCliArgsArray)) {
+        throw new Error('TEST_MOCHA_CLI_ARGS must be a valid JSON array of strings');
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      additionalOptions = loadOptions(mochaCliArgsArray) as Mocha.MochaOptions;
+    } catch (error) {
+      console.error('Failed to parse TEST_MOCHA_CLI_ARGS:', error);
+      throw error;
+    }
+  }
+
+  const mocha = new Mocha({
+    ...additionalOptions,
+    ...baseOptions, // The base options must take priority, as `loadOptions` returns the defaults as well.
   });
 
   const testsRoot = path.resolve(__dirname, '.');
