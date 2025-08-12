0a97926280c3f1bce59e2d1b04381e337cbdf7a8
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2025-08-12 15:15:43 +0200

    fix: script

--- .github/upload-to-s3.sh
+++ .github/upload-to-s3.sh
@@ -51,12 +51,12 @@ function uploadFile() {
 }
 
 # publish repo
-vsix_files=($SCRIPT_DIR/../*.vsix)
-if [ ${#vsix_files[@]} -ne 1 ]; then
+shopt -s nullglob  # makes unmatched globs expand to nothing
+vsix_files=("$SCRIPT_DIR"/../*.vsix)
+if [ "${#vsix_files[@]}" -ne 1 ]; then
   echo "Error: Expected to find exactly one .vsix file, but found ${#vsix_files[@]}." >&2
   exit 1
 fi
-
 FILENAME_SRC="snyk-vulnerability-scanner-preview.vsix"
 mv "${vsix_files[0]}" "$FILENAME_SRC"
 FILENAME_DST="$FILENAME_SRC"
